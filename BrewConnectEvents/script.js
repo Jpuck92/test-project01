82ee97165652854dbe50bcb800a0c0366b3d4b10
[jpuck704@gmail.com] <[jpuck704@gmail.com]>
2022-09-15 19:38:07 -0400

    BrewConnect

---
+++ BrewConnectEvents/script.js
@@ -0,0 +1,16 @@
+
+
+let event1 = {"name": "From the beginning", "dofw": "Thursday", "time": "5:30pm - 6:30pm", "img": "imgs/brwry.jp"};
+let event2 = {"name": "Intro to all-grain ", "dofw": "Firday", "time": "5:30pm - 6:30pm", "img": "imgs/brwry.jp"};
+let event3 = {"name": "Advance Techniques", "dofw": "Monday", "time": "2:30pm - 6:30pm", "img": "imgs/brwry.jp"};
+let event4 = {"name": "Advance Techniques", "dofw": "Monday", "time": "2:30pm - 6:30pm", "img": "imgs/brwry.jp"};
+
+let events = [event1, event2, event3, event4];
+// {"name": "Bob Saget - Imperial IPA", "dofw": "Thursday", "time": "5:30pm - 6:30pm", "img": "imgs/brwry.jp"},
+// {"name": "George Duffy - Helles Lager", "dofw": "Saturday", "time": "5:30pm - 6:30pm", "img": "imgs/brwry.jp"},
+// {"name": "Taylor Swift - Peanut Butter Stout", "dofw": "Thursday", "time": "5:30pm - 6:30pm", "img": "imgs/brwry.jp"},];
+
+
+const details = document.getElementById("details");
+// details.innerHTML = events[0].name;
+
